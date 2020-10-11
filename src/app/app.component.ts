79cd57490219ef792d80d0ac6f0d163fcfd16645
Stephen Vinuya <stephen.vinuya@gmail.com>
2020-10-11 13:38:03 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'jump-pack';
+}
